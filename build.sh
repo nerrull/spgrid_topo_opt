be2b93884770db4b11078986d4dc9bcb7a1d69ef
nerrull <rice1902@hpc-puget-necotis.gel.usherbrooke.ca>
2019-06-26 14:13:50 -0400

    update build script for puget

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export TC_MKL_PATH=~/intel/mkl/lib/intel64_lin/
-export CUDA_ARCH=60 # or 0 if there is no CUDA
+export TC_MKL_PATH=~/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin
+export CUDA_ARCH=61 # or 0 if there is no CUDA
 export TC_USE_DOUBLE=1 #
 ti build
